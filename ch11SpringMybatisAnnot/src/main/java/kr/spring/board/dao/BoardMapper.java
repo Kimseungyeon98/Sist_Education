f697aac32710eac4846d07d2828b512d7dd5ed6f
user <user@DESKTOP-JBKLJS2>
2024-06-21 16:41:31 +0900

    commit

---
+++ ch11SpringMybatisAnnot/src/main/java/kr/spring/board/dao/BoardMapper.java
@@ -0,0 +1,25 @@
+package kr.spring.board.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import kr.spring.board.vo.BoardVO;
+
+public interface BoardMapper {
+	// BoardMapper.xml에 작성
+	public void insertBoard(BoardVO board);
+	@Select("SELECT count(*) FROM aboard")
+	public int selectBoardCount();
+	// BoardMapper.xml에 작성	
+	public List<BoardVO> selectBoardList(Map<String,Integer> map);
+	@Select("SELECT * FROM aboard WHERE num = #{num}")
+	public BoardVO selectBoard(int num);
+	@Update("UPDATE aboard SET title=#{title}, writer=#{writer}, content=#{content} WHERE num=#{num}")
+	public void updateBoard(BoardVO board);
+	@Delete("DELETE FROM aboard WHERE num=#{num}")
+	public void deleteBoard(int num);
+}
